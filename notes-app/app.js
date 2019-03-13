d49a020eba8e21170f25585d0ed1a6289bce4ebb
CHAPEAU Nicolas <chapenicolas@gmail.com>
2019-03-13 22:50:35 +0100

    notes app npm require

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,10 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
+const email = validator.isEmail('email@gmail.com')
+const url = validator.isURL('http://welcome.com')
 
 console.log(msg)
+console.log(email)
+console.log(url)
