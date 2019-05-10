c90aeac43ae4038dde0b333da298e8157d1b31b3
CHAPEAU Nicolas <chapenicolas@gmail.com>
2019-05-10 10:30:27 +0200

    adding task filtering

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 
 // Tasks model
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -15,10 +15,13 @@ const Task = mongoose.model('Task', {
     owner: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
+        
         ref: 'User'
     }
+}, {
+    timestamps: true
 })
 
 
-
+const Task = mongoose.model('Task', taskSchema)
 module.exports = Task
